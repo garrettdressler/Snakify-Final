d2dfb1f66c0002340df71fefb45b1688089da3ec
Garrett Dressler <garrett.dressler@student.rjuhsd.us>
2018-10-22 21:26:43 +0000

    First Commit

---
+++ Snakeify Unit 1.py
@@ -0,0 +1,16 @@
+# Unit 1
+# Sum of three numbers
+a = int(input())
+b = int(input())
+c = int(input())
+print(a + b + c)
+
+#Hi John
+name = input()
+print('Hi ' + name)
+
+#Square
+a = int(input())
+print(a ** 2)
+
+# area of a right angled triangle
